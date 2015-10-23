a579d86d0166fef6bd4bd9d9d21dc122a05cac8f
Chien-Hong Liao <p12355668@gmail.com>
2015-10-24 02:32:59 +0800

    main base creat

--- ContactsPy/source/main.py
+++ ContactsPy/source/main.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 
-print "hello"
+exit = False
 
-file = open('wxgui.py', 'r', encoding='UTF-8')
-content = file.read()
-print(content)
-file.close()
+while not exit:
+    
+    print "歡迎使用通訊錄管理系統!\n"
+    print "*********************"
+    print "請選擇功能:"
+    print "1.開啟檔案"
+    print "2.離開"
+    print "*********************"
+    choice = raw_input("選擇代號:")
+
+    if choice == "1":
+        name = raw_input("請輸入檔案名稱:")
+        file = open(name,"r")
+        content = file.read()
+        print(content)
+        file.close()
+
+    elif choice == "2":
+        exit = True
+        print "\n謝謝您使用!\n"
+
