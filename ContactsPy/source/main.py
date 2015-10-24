934c8150c9baaca8aec10e104fc0728147731c31
Chien-Hong Liao <p12355668@gmail.com>
2015-10-25 00:27:10 +0800

    big change

--- ContactsPy/source/main.py
+++ ContactsPy/source/main.py
@@ -1,13 +1,90 @@
 #coding=utf-8
 
 import os
-import lib.person
 from lib.person import Person
+from lib.file import writeFile, readFile
+from xml.dom import minidom
+
+def editFile(persons):
+
+    exit = False
+
+    while not exit:
+        
+        print "歡迎使用通訊錄管理系統!"
+        print "*********************"
+        print "請選擇功能:"
+        print "1.View"
+        print "2.View All"
+        print "3.Add"
+        print "4.Remove"
+        print "5.Save"
+        print "6.Exit"
+        print "*********************"
+        
+        choice = raw_input("選擇代號:")
+        
+        if choice == "1":
+            view()
+
+        elif choice == "2":
+            viewAll()
+
+        elif choice == "3":
+            add()
+
+        elif choice == "4":
+            remove()
+
+        elif choice == "5":
+            save()
+
+        elif choice == "6":
+            exit = True
+
+def openFile():
+    
+    name = raw_input("請輸入檔案名稱:")
+    
+    if os.path.exists(name+".xml"):
+        
+        print (name + "檔案存在!\n")
+        persons = readFile(name)
+        
+        editFile(persons)
+
+    else:
+        
+        print "檔案不存在!\n"
+
+def creatFile():
+    
+    name = raw_input("請輸入檔案名稱:")
+    
+    if not os.path.exists(name+".xml"):
+
+        file = open(name+".xml","w")
+        print "已建立新檔案\n"
+        file.close()
+        
+    else:
+        print "建立失敗!有同名檔案存在!\n"
+
+def removeFile():
+
+    name = raw_input("請輸入欲移除檔案名稱:")
+    
+    if os.path.exists(name+".xml"):
+
+        os.remove(name)
+        print "移除檔案成功!\n"
+        
+    else:
+        print "失敗!檔案不存在!\n"
+
+# Main
 
 exit = False
-john = Person("John","Man","0912228","123456@gmail.com")
-john.name = "as"
-print john
 
 while not exit:
     
@@ -15,27 +92,27 @@ while not exit:
     print "*********************"
     print "請選擇功能:"
     print "1.開啟檔案"
-    print "2.離開"
+    print "2.建立新檔"
+    print "3.移除檔案"
+    print "4.離開"
     print "*********************"
     choice = raw_input("選擇代號:")
 
     if choice == "1":
-        name = raw_input("請輸入檔案名稱:")
-        if os.path.exists(name):
-            print (name + "檔案存在!\n")
-            file = open(name,"r")
-            content = file.read()
-            print(content)
-            file.close()
-        else:
-            print "檔案不存在!\n"
+        openFile()
 
     elif choice == "2":
-        exit = True
-        print "\n謝謝您使用!\n"
+        creatFile()
 
-def editFile():
-    a
+    elif choice == "3":
+        removeFile()
+
+    elif choice == "4":
+        exit = True
+
+print "\n謝謝您使用!\n"
+
+# Main End
 
 
 
